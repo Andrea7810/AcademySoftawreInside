5df58d2fe7cedf1646bdfaee85389f7dc3dd5a92
Andrea7810 <126260899+Andrea7810@users.noreply.github.com>
2023-03-22 08:57:32 +0100

    Add files via upload

---
+++ HelloWord/src/Starter.java
@@ -0,0 +1,30 @@
+
+public class Starter {
+
+	public static void main(String[] args) { 
+		
+	System.out.println("ciao a tutti");	
+	System.out.println("ciao");
+	System.out.println("ciao");
+	
+	//CONCETTO DI VARIABILE
+	
+	/*COMMENTO
+	 * VARIABILE INTERA
+	 * tipologia NomeDellaVariabile;
+	 */
+	
+	int somma; //init variabile
+	int numeroDiGiorniMancanti = 35; //init + assegnazione a valore
+	
+	somma = 66;
+	System.out.println(somma);
+
+	somma = 100;
+	
+	System.out.println(somma);
+	System.out.println("somma");
+
+	}
+
+}
